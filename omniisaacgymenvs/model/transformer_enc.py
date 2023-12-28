ae05e5ecef788d20d7d92c9f3b96e0e2b07f6f63
psh9002 <psh9002@gmail.com>
2023-12-29 00:24:08 +0900

    🚧 add dummy model class

--- omniisaacgymenvs/model/transformer_enc.py
+++ omniisaacgymenvs/model/transformer_enc.py
@@ -1,2 +1,37 @@
 import torch
 import torch.nn as nn
+from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
+# TODO: 여기에서는 point2를 통해 만든 feature를 받아서, 그것을 shared model에 넣어서 action을 만들어야 한다.
+
+# define shared model (stochastic and deterministic models) using mixins
+class SharedTransformerEnc(GaussianMixin, DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False,
+                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum"):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+        DeterministicMixin.__init__(self, clip_actions)
+
+        # TODO: self.net을 Transformer encoder로 바꿔야 한다.
+        self.net = nn.Sequential(nn.Linear(self.num_observations, 256),
+                                 nn.ELU(),
+                                 nn.Linear(256, 128),
+                                 nn.ELU(),
+                                 nn.Linear(128, 64),
+                                 nn.ELU())
+
+        self.mean_layer = nn.Linear(64, self.num_actions)
+        self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
+
+        self.value_layer = nn.Linear(64, 1)
+
+    def act(self, inputs, role):
+        if role == "policy":
+            return GaussianMixin.act(self, inputs, role)
+        elif role == "value":
+            return DeterministicMixin.act(self, inputs, role)
+
+    def compute(self, inputs, role):
+        if role == "policy":
+            return self.mean_layer(self.net(inputs["states"])), self.log_std_parameter, {}
+        elif role == "value":
+            return self.value_layer(self.net(inputs["states"])), {}
