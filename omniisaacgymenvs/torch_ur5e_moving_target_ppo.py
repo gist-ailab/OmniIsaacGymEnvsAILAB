31b40c37d467a937cd522058f3cc027aeff45a36
psh9002 <psh9002@gmail.com>
2024-01-02 03:18:59 +0900

    ♻️: disable preprocessor

--- omniisaacgymenvs/torch_ur5e_moving_target_ppo.py
+++ omniisaacgymenvs/torch_ur5e_moving_target_ppo.py
@@ -15,8 +15,8 @@ from skrl.resources.schedulers.torch import KLAdaptiveRL
 from skrl.trainers.torch import SequentialTrainer
 from skrl.utils import set_seed
 
-from omniisaacgymenvs.model.transformer_enc import SharedTransformerEnc
-# from omniisaacgymenvs.model.shared import Shared
+from omniisaacgymenvs.model.shared_transformer import SharedTransformerEnc
+from omniisaacgymenvs.model.shared import Shared
 
 # seed for reproducibility
 seed = 42
@@ -38,6 +38,7 @@ memory = RandomMemory(memory_size=16, num_envs=env.num_envs, device=device)
 # https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#models
 models = {}
 models["policy"] = SharedTransformerEnc(env.observation_space, env.action_space, device)
+# models["policy"] = Shared(env.observation_space, env.action_space, device)
 models["value"] = models["policy"]  # same instance: shared model
 
 
@@ -65,13 +66,13 @@ cfg["rewards_shaper"] = lambda rewards, timestep, timesteps: rewards * 0.01
 '''
 https://skrl.readthedocs.io/en/develop/api/resources/preprocessors/running_standard_scaler.html
 https://skrl.readthedocs.io/en/develop/intro/getting_started.html#preprocessors
+아래 전처리는 pointcloud랑 맞진 않는다. 필요하면 나는 별도로 구성해야 할듯.
 Preprocessor가 필요할 경우, 위의 `RunningStandardScaler`를 참고하여 pcd에 대한 preprocessor을 수행해봐도 될 것 같다.
-'''
-# cfg["state_preprocessor"] = RunningStandardScaler   # 이 preprocessor는 pointcloud랑 맞진 않는다. 필요하면 나는 별도로 구성해야 할듯.
+# cfg["state_preprocessor"] = RunningStandardScaler
 # cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
 # cfg["value_preprocessor"] = RunningStandardScaler
 # cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
-
+'''
 # logging to TensorBoard and write checkpoints (in timesteps)
 cfg["experiment"]["write_interval"] = 120
 cfg["experiment"]["checkpoint_interval"] = 1200
