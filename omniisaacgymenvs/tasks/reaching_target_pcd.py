c4be71ef2bdf0e208fa3ec38e5642d7c9f42a645
psh9002 <psh9002@gmail.com>
2024-02-29 13:14:00 +0900

    ♻️: change the camera coordination

--- omniisaacgymenvs/tasks/reaching_target_pcd.py
+++ omniisaacgymenvs/tasks/reaching_target_pcd.py
@@ -172,12 +172,12 @@ class PCDReachingTargetTask(RLTask):
         # point cloud view
         self.render_products = []
         # camera_positions = {0: [2, 1.5, 0.5],
-        camera_positions = {0: [2.3, 2.0, 0.5],
+        camera_positions = {0: [1.5, 1.5, 0.5],
                             1: [2, -1.3, 0.5],
                             2: [-0.5, -1.2, 0.5]}
                             # 2: [-1.5, -2.2, 0.5]}
                             # 2: [-4.8, -6.1, 0.5]}
-        camera_rotations = {0: [0, -10, 60],
+        camera_rotations = {0: [0, -10, 50],
                             1: [0, -10, -45],
                             2: [0, -10, -130]}
         env_pos = self._env_pos.cpu()
