d9e0641a1567007bb078cf5a6221d89e286e97b3
psh9002 <psh9002@gmail.com>
2024-03-29 01:55:31 +0900

    ♻️: check converted result

--- omniisaacgymenvs/utils/check_ply.py
+++ omniisaacgymenvs/utils/check_ply.py
@@ -4,8 +4,8 @@ import open3d.core as o3c
 import numpy as np
 
 print("Testing IO for point cloud ...")
-pcd = o3d.io.read_point_cloud("/home/bak/Documents/tool/pcd_tool.ply")
-# pcd = o3d.io.read_point_cloud("/home/bak/Documents/035_power_drill/pcd_power_drill.ply")
+pcd = o3d.io.read_point_cloud("/home/bak/.local/share/ov/pkg/isaac_sim-2023.1.1/OmniIsaacGymEnvs/omniisaacgymenvs/robots/articulations/ur5e_tool/usd/cylinder/pcd_cylinder.ply")
+# pcd = o3d.io.read_point_cloud("/home/bak/Documents/tool/pcd_tool.ply")
 
 org_coord = o3d.geometry.TriangleMesh().create_coordinate_frame(size=0.2, origin=np.array([0, 0, 0]))
 o3d.visualization.draw_geometries([pcd, org_coord], window_name=f'point cloud')
