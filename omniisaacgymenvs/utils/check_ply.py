18f116ee51e5294631acea97063b6b7a0950fcb5
bakseongho <psh9002@gmail.com>
2024-06-25 09:59:41 +0900

    ✨: check created point cloud

--- omniisaacgymenvs/utils/check_ply.py
+++ omniisaacgymenvs/utils/check_ply.py
@@ -4,7 +4,8 @@ import open3d.core as o3c
 import numpy as np
 
 print("Testing IO for point cloud ...")
-pcd = o3d.io.read_point_cloud("/home/bak/.local/share/ov/pkg/isaac_sim-2023.1.1/OmniIsaacGymEnvs/omniisaacgymenvs/robots/articulations/ur5e_tool/usd/cylinder/pcd_cylinder.ply")
+name="ladle"
+pcd = o3d.io.read_point_cloud(f"/home/bak/.local/share/ov/pkg/isaac_sim-2023.1.1/OmniIsaacGymEnvs/omniisaacgymenvs/robots/articulations/ur5e_tool/usd/tool/{name}/{name}.ply")
 # pcd = o3d.io.read_point_cloud("/home/bak/Documents/tool/pcd_tool.ply")
 
 org_coord = o3d.geometry.TriangleMesh().create_coordinate_frame(size=0.2, origin=np.array([0, 0, 0]))
