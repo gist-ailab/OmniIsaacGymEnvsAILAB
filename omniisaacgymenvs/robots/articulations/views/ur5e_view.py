f970f9fc26fe36ed69ecee3084ec12566408f803
bakseongho <psh9002@gmail.com>
2024-06-30 22:01:13 +0900

    ✨: add sub class variables for the flange and the tool

--- omniisaacgymenvs/robots/articulations/views/ur5e_view.py
+++ omniisaacgymenvs/robots/articulations/views/ur5e_view.py
@@ -4,5 +4,22 @@ from omni.isaac.core.articulations import ArticulationView
 from omni.isaac.core.prims import RigidPrimView
 
 class UR5eView(ArticulationView):
-    def __init__(self, prim_paths_expr: str, name: str = "robot_view") -> None:
+    def __init__(self,
+                 prim_paths_expr: str,
+                 name: str = "ur5e_tool_view") -> None:
         super().__init__(prim_paths_expr=prim_paths_expr, name=name, reset_xform_properties=False)
+
+        robot_name = name.split("_")
+        robot_name = robot_name[0] + "_" + robot_name[1]
+
+        self._flanges = RigidPrimView(
+            prim_paths_expr=f"/World/envs/.*/{robot_name}/tool0",
+            name=f"{robot_name}_flange_view",
+            reset_xform_properties=False
+        )
+        
+        self._tools = RigidPrimView(
+            prim_paths_expr=f"/World/envs/.*/{robot_name}/tool",
+            name=f"{robot_name}_tool_view",
+            reset_xform_properties=False
+        )
